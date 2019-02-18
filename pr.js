3e7c9e7848b01659caaa410ecbaa52492cb710cd
James Treanor <jtreanor3@gmail.com>
2019-02-18 11:53:27 +0000

    Improved async handling

--- pr.js
+++ pr.js
@@ -1,45 +1,50 @@
-import {warn, fail, danger} from "danger"
+/*jshint esversion: 8 */
 
-export default async () => {
-    if (danger.github == null || danger.github.pr == null) {
-        fail("Not running on a Github PR.");
-        return;
-    }
+import {warn, fail, danger} from "danger";
 
-    const githubLabels = danger.github.issue.labels
+if (danger.github == null || danger.github.pr == null) {
+    fail("Not running on a Github PR.");
+    return;
+}
 
-    // A PR should have at least one label
-    if (githubLabels.count == 0) {
-        warn("PR is missing at least one label.");
-    }
+const githubLabels = danger.github.issue.labels;
 
+// A PR should have at least one label
+if (githubLabels.length == 0) {
+    warn("PR is missing at least one label.");
+}
+
+(async () => {
     // Warn if the PR doesn't have a milestone
-    const issue = await danger.github.api.issues.get(danger.github.thisPR)
+    const issue = await danger.github.api.issues.get(danger.github.thisPR);
     if (issue.data.milestone == null) {
         warn("PR is not assigned to a milestone.");
     }
+})();
 
-    // A PR shouldn't be merged with the 'DO NOT MERGE' label
-    for (let label of githubLabels) {
-        if (label.name.includes("DO NOT MERGE")) {
-            warn("This PR is tagged with 'DO NOT MERGE'.");
-            break;
-        }
+
+// A PR shouldn't be merged with the 'DO NOT MERGE' label
+for (let label of githubLabels) {
+    if (label.name.includes("DO NOT MERGE")) {
+        warn("This PR is tagged with 'DO NOT MERGE'.");
+        break;
     }
+}
 
-    // Warn when there is a big PR
-    if (danger.github.pr.additions + danger.github.pr.deletions > 500) {
-        warn("PR has more than 500 lines of code changing. Consider splitting into smaller PRs if possible.");
-    }
+// Warn when there is a big PR
+if (danger.github.pr.additions + danger.github.pr.deletions > 500) {
+    warn("PR has more than 500 lines of code changing. Consider splitting into smaller PRs if possible.");
+}
 
-    // Core Data Model Safety Checks
-    const target_release_branch = danger.github.pr.base.ref.startsWith("release/");
-    const changedFiles = danger.git.modified_files.concat(danger.git.deleted_files).concat(danger.git.added_files);
-    const has_modified_model = changedFiles.some(path => path.includes(".xcdatamodeld"));
-    if (target_release_branch && has_modified_model) {
-        warn("Core Data: Do not edit an existing Core Data model in a release branch unless it hasn't been released to testers yet. Instead create a new model version and merge back to develop soon.");
-    }
+// Core Data Model Safety Checks
+const target_release_branch = danger.github.pr.base.ref.startsWith("release/");
+const changedFiles = danger.git.modified_files.concat(danger.git.deleted_files).concat(danger.git.created_files);
+const has_modified_model = changedFiles.some(path => path.includes(".xcdatamodeld"));
+if (target_release_branch && has_modified_model) {
+    warn("Core Data: Do not edit an existing Core Data model in a release branch unless it hasn't been released to testers yet. Instead create a new model version and merge back to develop soon.");
+}
 
+(async () => {
     // Podfile should not reference commit hashes
     if (danger.git.modified_files.includes("Podfile")) {
         const diff = await danger.git.diffForFile("Podfile");
@@ -47,4 +52,4 @@ export default async () => {
             warn("Podfile: reference to a commit hash");
         }
     }
-}
+})();
