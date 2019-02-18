864f6e116e6c7e85b5364c5422f82ae2ed746423
James Treanor <jtreanor3@gmail.com>
2019-02-18 11:55:50 +0000

    Don't check Podfile

--- pr.js
+++ pr.js
@@ -44,12 +44,12 @@ if (target_release_branch && has_modified_model) {
     warn("Core Data: Do not edit an existing Core Data model in a release branch unless it hasn't been released to testers yet. Instead create a new model version and merge back to develop soon.");
 }
 
-(async () => {
-    // Podfile should not reference commit hashes
-    if (danger.git.modified_files.includes("Podfile")) {
-        const diff = await danger.git.diffForFile("Podfile");
-        if (/\+[^#]*:commit/.test(diff.added)) {
-            warn("Podfile: reference to a commit hash");
-        }
-    }
-})();
+// (async () => {
+//     // Podfile should not reference commit hashes
+//     if (danger.git.modified_files.includes("Podfile")) {
+//         const diff = await danger.git.diffForFile("Podfile");
+//         if (/\+[^#]*:commit/.test(diff.added)) {
+//             warn("Podfile: reference to a commit hash");
+//         }
+//     }
+// })();
