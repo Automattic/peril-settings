d8ea523b2fb63c771f7b896b5b5533e078adbd51
James Treanor <jtreanor3@gmail.com>
2019-06-10 15:52:46 +0100

    Divide common.ts so that Simplenote can use only the label rules

---
+++ org/github/label.ts
@@ -0,0 +1,16 @@
+import {warn, danger} from "danger";
+
+export default async () => {
+    const githubLabels = danger.github.issue.labels;
+
+    // A PR should have at least one label
+    if (githubLabels.length == 0) {
+        warn("PR is missing at least one label.");
+    }
+
+    // A PR shouldn't be merged with the 'DO NOT MERGE' label
+    const doNotMerge = githubLabels.some(label => label.name.includes("DO NOT MERGE"));
+    if (doNotMerge) {
+        warn("This PR is tagged with 'DO NOT MERGE'.");
+    }
+};
