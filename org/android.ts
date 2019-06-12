dd582d196af40d81e733c6470eedd7db5780a2af
Mark Parris <mparris@chefsteps.com>
2019-06-11 17:01:54 -0700

    Commit new files to merge branch
    Commit new files to merge branch
    
    Commit new files to merge branch
    
    Commit new files to merge branch
    
    Commit new files to merge branch
    
    Commit new files to merge branch
    
    Commit new files to merge branch
    
    Commit new files to merge branch
    
    Commit new files to merge branch
    
    Commit new files to merge branch
    
    Commit new files to merge branch
    
    Commit new files to merge branch

--- org/android.ts
+++ org/android.ts
@@ -28,6 +28,8 @@ export default async () => {
 
         const api = danger.github.api;
         const pr = danger.github.thisPR;
+        const utils = danger.github.utils;
+
         const mergeBranch = `merge/${pr.repo}/${pr.number}`;
         let mergeBranchHead: any;
         let destRepoHead: any;
@@ -37,13 +39,12 @@ export default async () => {
             console.log("Modified files: " + modifiedFiles.toString());
             console.log("Created files: " + createdFiles.toString());
             console.log("Deleted files: " + deletedFiles.toString());
-            // console.log(`PR's mergeable status is: ${pr.mergeable}`);
 
             // Look for an existing merge branch.
             console.log(`Looking for an existing merge branch: ${mergeBranch}`);
             try {
                 mergeBranchHead = (await api.gitdata.getReference({owner: org, repo: destRepo, ref: `heads/${mergeBranch}`})).data.object.sha;
-                console.log(`mergeBranchHead is ${mergeBranchHead}`);
+                console.log(`Found ${mergeBranch}: SHA ${mergeBranchHead}`);
             }
             catch (e) {
                 // The REST API returns a 404 if the request ref does not exist.
@@ -51,16 +52,15 @@ export default async () => {
                     console.log(`Branch ${mergeBranch} not found, creating...`);
                     mergeBranchHead = null;
                 }
+                // Otherwise, not ours, so rethrow the error.
                 else {
-                    console.log("### MPTEST ###");
-                    console.log(JSON.stringify(e));
                     throw(e);
                 }
             }
             
             // If we didn't find an existing merge branch, create a new one.
             if (mergeBranchHead == null) {
-                // Get HEAD for ${destRepoBranch}
+                // Get HEAD for destination branch.
                 console.log(`Getting refs/heads/${destRepoBranch} for ${org}/${destRepo}`);
                 destRepoHead = (await api.gitdata.getReference({owner: org, repo: destRepo, ref: `heads/${destRepoBranch}`})).data.object.sha;
                 console.log(`Got refs/heads/${destRepoBranch} for ${org}/${destRepo}: SHA ${destRepoHead}`);
@@ -69,19 +69,33 @@ export default async () => {
                 // (i.e. `git checkout -b`.)
                 console.log(`Creating merge branch: ${mergeBranch}`);
                 mergeBranchHead = (await api.gitdata.createReference({owner: org, repo: destRepo, ref: `refs/heads/${mergeBranch}`, sha: destRepoHead})).data.object.sha;
-                console.log(`Created ${mergeBranch}, SHA ${mergeBranchHead}`);
+                console.log(`Created ${mergeBranch}: SHA ${mergeBranchHead}`);
             }
 
             // Apply changes
+            console.log("Apply changes to merge branch");
+            
+            for (let file of createdFiles) {
+                console.log(`Processing new file ${file}`);
+                console.log("Getting contents...");
+                let fileContents = await utils.fileContents(file);
+                let encodedContents = Buffer.from(fileContents).toString('base64');
+                console.log(`Encoded contents: ${encodedContents.substr(0,15)}...`);
+                
+                console.log(`Creating ${file} in ${mergeBranch}`);
+                let commitSha = (await api.repos.createFile({owner: org, repo: destRepo, path: file, branch: mergeBranch, content: encodedContents, message: `Auto-commit by Peril`})).data.commit.sha;
+                console.log(`Created ${file}: SHA ${commitSha}`)
+            }
+
+            // Create PR
+            // Comment on PR 
+            // Poll for mergeability
+            // Comment on PR, fail if there are conflicts
+
         }
         catch (e) {
             console.error(`!!! Caught error: ${e.message}`);
             console.log(JSON.stringify(e));
         }
-            
-        // Create PR
-        // Comment on PR 
-        // Poll for mergeability
-        // Comment on PR, fail if there are conflicts
     }
 };
