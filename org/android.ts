56dbdd39ba7b60ef77d69687a0e4bad4124b1c49
Mark Parris <mparris@chefsteps.com>
2019-06-14 08:44:08 -0700

    Create rule to post merge instructions

--- org/android.ts
+++ org/android.ts
@@ -1,4 +1,4 @@
-import {message, warn, danger} from "danger";
+import {warn, danger} from "danger";
 
 export default async () => {
 
@@ -9,7 +9,5 @@ export default async () => {
     if (hasModifiedReleaseNotes && !hasModifiedPlayStoreStrings) {
         warn("The PlayStoreStrings.po file must be updated any time changes are made to release notes");
     }
-
-    message("### MPTEST ###");
 
 };
