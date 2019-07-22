ad5aece97002ee6044a61dedd39d4eb4a4ceb634
James Treanor <jtreanor3@gmail.com>
2019-07-22 12:42:26 +0100

    Move existing files into org/pr subdirectory

--- org/common.ts
+++
@@ -1,8 +0,0 @@
-import {fail, danger} from "danger";
-
-export default async () => {
-    if (danger.github == null || danger.github.pr == null) {
-        fail("Not running on a Github PR.");
-        return;
-    }
-};
