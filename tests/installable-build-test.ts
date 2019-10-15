f280444f7b6f4abe57cc162101216d27406d2bef
James Treanor <jtreanor3@gmail.com>
2019-10-15 12:15:06 +0100

    Merge android-installable-build.ts and force-circleci-status.ts to share installable build logic

---
+++ tests/installable-build-test.ts
@@ -0,0 +1,154 @@
+
+import fetch from 'node-fetch'
+jest.mock('node-fetch', () => jest.fn())
+
+jest.mock("danger", () => jest.fn());
+import danger from "danger";
+const dm = danger as any;
+
+import installableBuild from "../org/pr/installable-build";
+
+let mockedCommentJson = {}
+let mockedArtifacts = []
+
+beforeEach(() => {
+    fetch.mockImplementation((url) => {
+      let result: any = mockedArtifacts
+      if (url.includes('comment.json')) {
+        result = mockedCommentJson
+      }
+      return {
+          ok: true,
+          json: () => result
+      }
+    })
+
+    dm.danger = {
+        github: {
+          api: {
+            repos: {
+                createStatus: jest.fn(),
+            },
+            search: {
+              issuesAndPullRequests: jest.fn(),
+            },
+            issues: {
+              createComment: jest.fn(),
+              listComments: jest.fn()
+            },
+            pulls: {
+              get: jest.fn(),
+            }
+          },
+        },
+      }
+
+    ;(global as any).console = {
+        log: jest.fn()
+    }
+
+    // Mock Github API for posting comments
+    // Gets a corresponding issue
+    dm.danger.github.api.search.issuesAndPullRequests.mockReturnValueOnce(Promise.resolve({ data: { items: [{ number: 1 }] } }))
+    // No existing comments
+    dm.danger.github.api.issues.listComments.mockReturnValueOnce(Promise.resolve({data: []}))
+    // Returns a PR with the right commit
+    dm.danger.github.api.pulls.get.mockReturnValueOnce(
+        Promise.resolve({ data: { head: { sha: "abc" } } })
+    )
+    // Mock commenting on the PR
+    dm.danger.github.api.issues.createComment.mockReturnValueOnce(Promise.resolve({data: { html_url: "https://github.com/comment_url" }}))
+})
+
+const expectComment = (webhook, commentBody) => {
+  expect(dm.danger.github.api.issues.createComment).toBeCalledWith({
+    owner: webhook.repository.owner.login,
+    repo: webhook.repository.name,
+    number: 1,
+    body: `<!--- Installable Build Comment --->\n${commentBody}`
+  })
+  expect(console.log).toBeCalledWith('Installable build comment posted to https://github.com/comment_url')
+}
+
+describe("installable build handling", () => {
+    it("bails when its not a status/state we want to handle", async () => {
+        await installableBuild({ state: "fail", context: "Failure context" } as any)
+        expect(console.log).toBeCalledWith("Not a status we want to process for installable builds - got 'Failure context' (fail)")
+
+        await installableBuild({ state: "success", context: "ci/circleci: Installable Build/Hold" } as any)
+        expect(console.log).toBeCalledWith("Not a status we want to process for installable builds - got 'ci/circleci: Installable Build/Hold' (success)")
+    })
+
+    it("updates the status to be 'success' when it is the right context, and comments", async () => {
+        const webhook: any = {
+            state: "pending",
+            context: "ci/circleci: Installable Build/Hold",
+            description: "Holding build",
+            target_url: "https://circleci.com/workflow-run/abcdefg",
+            repository: {
+                name: 'Repo',
+                owner: { login: 'Owner' }
+            },
+            commit: { sha: 'abc' }
+        }
+        await installableBuild(webhook)
+
+        expect(dm.danger.github.api.repos.createStatus).toBeCalledWith({
+            owner: webhook.repository.owner.login,
+            repo: webhook.repository.name,
+            state: "success",
+            context: webhook.context,
+            description: webhook.description,
+            target_url: webhook.target_url,
+        })
+
+        expectComment(webhook, `You can trigger an installable build for these changes by visiting CircleCI [here](${webhook.target_url}).`)
+    })
+
+    it("Posts a download comment with the content of comment.json", async () => {
+      mockedArtifacts = [{
+        path: 'comment.json',
+        url: 'https://circleci.com/comment.json'
+      }]
+      mockedCommentJson = {
+        body: 'Mocked download comment.'
+      }
+
+      const webhook: any = {
+        state: "success",
+        context: "ci/circleci: Installable Build",
+        description: "Building",
+        target_url: "https://circleci.com/gh/Owner/Repo/12345?some=query",
+        repository: {
+            name: 'Repo',
+            owner: { login: 'Owner' }
+        },
+        commit: { sha: 'abc' }
+      }
+      await installableBuild(webhook)
+
+      expectComment(webhook, "Mocked download comment.")
+    })
+
+    it("Posts a download comment linking to an APK", async () => {
+      mockedArtifacts = [{
+        path: 'file.apk',
+        url: 'https://circleci.com/file.apk'
+      }]
+
+      const webhook: any = {
+        state: "success",
+        context: "ci/circleci: Installable Build",
+        description: "Building",
+        target_url: "https://circleci.com/gh/Owner/Repo/12345?some=query",
+        repository: {
+            name: 'Repo',
+            owner: { login: 'Owner' }
+        },
+        commit: { sha: 'abc' }
+      }
+      await installableBuild(webhook)
+
+      expectComment(webhook, `You can test the changes on this Pull Request by downloading the APK [here](${mockedArtifacts[0].url}).`)
+    })
+})
