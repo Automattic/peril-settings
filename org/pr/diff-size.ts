5e09798b34b8a77a263186ee80588ea81eb1c56c
Lorenzo Mattei <lore.mattei@gmail.com>
2020-02-13 16:02:22 +0100

    Skip diff size and milestone checks for release PRs

--- org/pr/diff-size.ts
+++ org/pr/diff-size.ts
@@ -1,6 +1,16 @@
 import {warn, danger} from "danger";
 
 export default async () => {
+    // Skip for release PRs
+    const githubLabels = danger.github.issue.labels;
+
+    if (githubLabels.length != 0) {
+        const releases = githubLabels.some(label => label.name.includes("Releases"));
+        if (releases) {
+            return;
+        }
+    }
+    
     // Warn when there is a big PR
     if (danger.github.pr.additions + danger.github.pr.deletions > 500) {
         warn("PR has more than 500 lines of code changing. Consider splitting into smaller PRs if possible.");
--- org/pr/milestone.ts
+++ org/pr/milestone.ts
@@ -1,6 +1,16 @@
 import {warn, danger} from "danger";
 
 export default async () => {
+    // Skip for release PRs
+    const githubLabels = danger.github.issue.labels;
+
+    if (githubLabels.length != 0) {
+        const releases = githubLabels.some(label => label.name.includes("Releases"));
+        if (releases) {
+            return;
+        }
+    }
+  
     // Warn if the PR doesn't have a milestone
     const issue = await danger.github.api.issues.get(danger.github.thisPR);
     if (issue.data.milestone == null) {
