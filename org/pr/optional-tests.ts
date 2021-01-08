4ac9fba9c68810e79fd1101ca96c717829cc2b89
Oguz Kocer <oguz.kocer@automattic.com>
2021-01-08 16:28:44 -0500

    Adds fluxc/Optional Tests to optional tests

--- org/pr/optional-tests.ts
+++ org/pr/optional-tests.ts
@@ -5,7 +5,11 @@ const CIRCLECI_TOKEN: string = process.env['CIRCLECI_TOKEN']
 const PERIL_BOT_USER_ID: number = parseInt(process.env['PERIL_BOT_USER_ID'], 10)
 
 // This is a list of the CircleCI statuses to process
-const HOLD_CONTEXTS: string[] = ["ci/circleci: Optional Tests/Hold", "ci/circleci: wordpress_ios/Optional Tests"]
+const HOLD_CONTEXTS: string[] = [
+    "ci/circleci: Optional Tests/Hold",
+    "ci/circleci: wordpress_ios/Optional Tests",
+    "ci/circleci: fluxc/Optional Tests"
+]
 
 async function markStatusAsSuccess(status) {
   console.log(`Updating ${status.context} state to be success`)
