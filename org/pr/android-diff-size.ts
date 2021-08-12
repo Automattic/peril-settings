4a827bed671820fd35f149671c7092abbb6b52cd
Lorenzo Mattei <lore.mattei@gmail.com>
2021-08-12 12:31:14 +0200

    Add the number of lines of code changes

--- org/pr/android-diff-size.ts
+++ org/pr/android-diff-size.ts
@@ -14,23 +14,17 @@ export default async () => {
     
     // Calculate the changes to Test files because we don't want to limit those
     const modifiedStrings = modifiedFiles.filter((path: string) => path.includes("/src/test"));
-    let msg = ""
     let changesToTests = 0;
     for (let file of modifiedStrings) {        
-        msg = msg + file + "\r\n";
         const stringDiffs = await danger.git.diffForFile(file)
-        msg = msg + `\`${stringDiffs.added}\`\r\n` + `\`${stringDiffs.removed}\``;
-        
-        let addedStrings = stringDiffs.added.split("\n");
-        let addedLines = addedStrings.filter(x => x.startsWith("+")).length;
-
-        let removedLines = stringDiffs.removed.split("\n").filter(x => x.startsWith("-")).length;
+        const addedLines = stringDiffs.added.split("\n").filter(x => x.startsWith("+")).length;
+        const removedLines = stringDiffs.removed.split("\n").filter(x => x.startsWith("-")).length;
         changesToTests += addedLines + removedLines;
     }
 
     // Warn when there is a big PR
     let codeChanges = danger.github.pr.additions + danger.github.pr.deletions - changesToTests;
     if (codeChanges > 300) {
-        warn(`PR has more than 300 lines of code changing. Consider splitting into smaller PRs if possible. ([${codeChanges} - ${msg}])`);
+        warn(`PR has more than 300 lines of code changing. Consider splitting into smaller PRs if possible. ([${codeChanges} lines of code changes])`);
     }
 };
