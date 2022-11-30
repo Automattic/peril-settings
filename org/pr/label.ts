d65fc748b10336643b846fd3d6d2ce0ca1705f1a
Olivier Halligon <olivier.halligon@automattic.com>
2022-11-30 19:12:03 +0100

    Block WPAndroid PRs tagged as 'Not Ready for Merge'
    WPAndroid repos use a different name for that kind of label, with 'not ready for merge' rather than 'do not merge' wording, so we had to take that into account too.

--- org/pr/label.ts
+++ org/pr/label.ts
@@ -8,8 +8,13 @@ export default async () => {
         warn("PR is missing at least one label.");
     }
 
+    function is_do_not_merge_label(label) {
+        var lc_label = label.name.toLowerCase();
+        return lc_label.includes("do not merge") || lc_label.includes("not ready for merge");
+    }
+
     // A PR shouldn't be merged with the 'DO NOT MERGE' label
-    const doNotMergeLabel = githubLabels.find(label => label.name.toLowerCase().includes("do not merge"));
+    const doNotMergeLabel = githubLabels.find(is_do_not_merge_label);
     if (doNotMergeLabel) {
         fail(`This PR is tagged with '${doNotMergeLabel.name}' label.`);
     }
